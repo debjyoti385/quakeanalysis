60efd9c2fde59ccc9a35cc806fd714a599b0b64c
Debjyoti Paul <debjyotipaul385@gmail.com>
2015-12-03 11:00:37 -0700

    added sliders

---
+++ webUI/js/sliders.js
@@ -0,0 +1,91 @@
+
+// Create a new date from a string, return as a timestamp.
+function timestamp(str){
+    return new Date(str).getTime();
+}
+
+var dateSlider = document.getElementById('date-slider');
+
+noUiSlider.create(dateSlider, {
+// Create two timestamps to define a range.
+    range: {
+        min: timestamp('2005'),
+        max: timestamp('2016')
+    },
+
+// Steps of one week
+    step: 7 * 24 * 60 * 60 * 1000,
+
+// Two more timestamps indicate the handle starting positions.
+    start: [ timestamp('2011'), timestamp('2015') ],
+
+// No decimals
+    format: wNumb({
+        decimals: 0
+    }),
+    connect: true
+});
+
+// Create a list of day and monthnames.
+var
+    weekdays = [
+        "Sunday", "Monday", "Tuesday",
+        "Wednesday", "Thursday", "Friday",
+        "Saturday"
+    ],
+    months = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+
+// Append a suffix to dates.
+// Example: 23 => 23rd, 1 => 1st.
+function nth (d) {
+    if(d>3 && d<21) return 'th';
+    switch (d % 10) {
+        case 1:  return "st";
+        case 2:  return "nd";
+        case 3:  return "rd";
+        default: return "th";
+    }
+}
+
+// Create a string representation of the date.
+function formatDate ( date ) {
+    return weekdays[date.getDay()] + ", " +
+        date.getDate() + nth(date.getDate()) + " " +
+        months[date.getMonth()] + " " +
+        date.getFullYear();
+}
+
+var dateValues = [
+    document.getElementById('event-start'),
+    document.getElementById('event-end')
+];
+
+dateSlider.noUiSlider.on('update', function( values, handle ) {
+    dateValues[handle].innerHTML = formatDate(new Date(+values[handle]));
+});
+
+var slider = document.getElementById('magnitude_slider');
+
+noUiSlider.create(slider, {
+    start: [ 1, 10 ], // Handle start position
+    step: 0.1, // Slider moves in increments of '10'
+    margin: 0.3, // Handles must be more than '20' apart
+    connect: true, // Display a colored bar between the handles
+    direction: 'rtl', // Put '0' at the bottom of the slider
+    behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+    range: { // Slider can select '0' to '100'
+        'min': 1,
+        'max': 10
+    },
+    tooltips: [  wNumb({ decimals: 1 }), wNumb({ decimals: 1 }) ],
+    pips: { // Show a scale with the slider
+        mode: 'count',
+        values: 10,
+        density: 1
+    }
+});
